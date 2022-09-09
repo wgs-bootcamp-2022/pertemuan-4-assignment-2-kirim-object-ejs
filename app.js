03fc609a70875cf2bffdb30d42afb9acdbacac4d
zakariyahya <yazakkars@gmail.com>
2022-09-09 11:27:09 +0700

    assignment 2 kirim object ejs

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req,res)=>{
+    res.render('index',{
+        name : "Yahya",
+        title:"Web yahya"
+    })
+})
+app.get('/about', (req,res)=>{
+    res.render('about')
+})
+app.get('/contact', (req,res)=>{
+    cont = [
+        { name : "Yahya",
+          email: "Yahyaz@gmail.com"
+        },
+        { name : "Budi",
+          email: "Budi@gmail.com"
+        },        
+        { name : "Zack",
+          email: "Zack@gmail.com"
+        },
+    ]
+    res.render('contact',{cont})
+})
+
+app.get('/product/:id', (req,res)=>{
+    const id = req.params.id
+    const category = req.query.category
+    // res.send(`product id: ${id} <br> category id: ${category}`)
+    res.render('product.ejs', category)
+})
+
+app.get('/category/:id', (req,res)=>{
+    res.send('category id :'+req.params.id)
+})
+app.get('/product/:id/:category_id', (req,res)=>{
+    res.send('product id :'+req.params.id+' category id :'+req.params.category_id)
+})
+app.use('/', (req, res) => {
+    res.status(404)
+    res.send('page not found: 404')
+})
+app.listen(port, ()=> {
+    console.log(`Example app listening on port ${port}`)
+})
